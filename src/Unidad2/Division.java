926ae7fd67961ff148184cc61865c3ba70d86fae
Nachofv <nachin.ifv@gmail.com>
2019-11-12 20:26:13 +0100

    Ejercicio1 de la Unidad 2 resuelto

---
+++ src/Unidad2/Division.java
@@ -0,0 +1,26 @@
+package Unidad2;
+
+public class Division {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*
+		 * En el método main de una clase Java llamada Division escribe un programa que
+		 * muestre en la consola la división de 1234 entre 532 siendo ambos números
+		 * reales. El formato de salida será un número que ocupará un mínimo de 15
+		 * caracteres en pantalla, de los cuales dos se utilizarán para la parte
+		 * decimal.
+		 */
+
+		double x = 1234;
+
+		double y = 532;
+
+		double resultado = (x / y);
+
+		System.out.printf("%1.2f", resultado);
+
+	}
+
+}
